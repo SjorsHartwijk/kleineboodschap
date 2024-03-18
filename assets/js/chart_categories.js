c13e4e1a486cc167f3168a7789c18c957d429793
Sjors Hartwijk <s.hartwijk@gmail.com>
2024-03-18 15:44:56 +0100

    donut chart geeft nu het aantal afleveringen weer per stuk ipv totaalsum van minuten per categorie

--- assets/js/chart_categories.js
+++ assets/js/chart_categories.js
@@ -10,37 +10,34 @@ function loadCategories() {
         return response.json();
     })
     .then(data => {
-        // Object om categorieën en bijbehorende tijden bij te houden
-        const categories = {};
-        // Loop door de afleveringen om categorieën en tijden te bundelen
+        // Object om het aantal afleveringen per categorie bij te houden
+        const episodesPerCategory = {
+            'nieuws': 0,
+            'achtergrond': 0,
+            'reportage': 0,
+            'interview': 0
+        };
+
+        // Loop door de afleveringen om ze op te tellen per categorie
         data.forEach(episode => {
             const category = episode.categorie;
-            if (category in categories) {
-                categories[category] += parseFloat(episode.tijd); // Tijd toevoegen aan bestaande categorie
-            } else {
-                categories[category] = parseFloat(episode.tijd); // Nieuwe categorie maken
-            }
+            episodesPerCategory[category]++;
         });
-        // Kleuren toewijzen aan categorieën
-        const categoryColors = {
-            'nieuws': '#F6CA83',
-            'achtergrond': '#7B9F93',
-            'reportage': '#4685AE',
-            'interview': '#C46868'
-        };
-        // Labels en data voor het cirkeldiagram
-            const labels = Object.keys(categories);
-            const dataValues = Object.values(categories);
-            const backgroundColors = labels.map(category => categoryColors[category]);
+
+        // Labels en data voor het donut chart
+        const labels = Object.keys(episodesPerCategory);
+        const datalabel = labels.map(label => episodesPerCategory[label]);
+        const backgroundColors = labels.map(label => categoryColors[label]);
             // Creeër een canvas voor het cirkeldiagram
-            const ctx = document.getElementById('pieChart').getContext('2d');
+            const ctx = document.getElementById('donutChart').getContext('2d');
             // Creeër het cirkeldiagram
             const pieChart = new Chart(ctx, {
                 type: 'doughnut',
                 data: {
                     labels: labels,
                     datasets: [{
-                        data: dataValues,
+                        label: 'Aantal afleveringen',
+                        data: datalabel,
                         backgroundColor: backgroundColors
                     }]
                 },
