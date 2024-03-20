a48114bd370da30f4e39c80b788439d96a7a9d5f
Sjors Hartwijk <s.hartwijk@gmail.com>
2024-03-20 10:48:20 +0100

    statistiekenkaarten uitegebreid met uren data

--- assets/js/category_statistics.js
+++ assets/js/category_statistics.js
@@ -9,6 +9,14 @@ function calculateNewsStatistics() {
     .then(data => {
         const newsEpisodes = data.filter(episode => episode.categorie === 'nieuws');
         const totalNewsEpisodes = newsEpisodes.length;
+        let totalNewsMinutes = 0;
+
+        // Loop door de nieuwsafleveringen en tel de tijd in minuten op
+        newsEpisodes.forEach(episode => {
+            totalNewsMinutes += parseInt(episode.tijd);
+        });
+        // Bereken het totale aantal uren
+        const totalNewsHours = totalNewsMinutes / 60;
 
         const totalEpisodes = data.length;
         const newsPercentage = (totalNewsEpisodes / totalEpisodes) * 100;
@@ -17,8 +25,9 @@ function calculateNewsStatistics() {
         const newsStatisticsElement = document.getElementById('newsStatistics');
         newsStatisticsElement.innerHTML = `
             <h2 class="h5 card-title">Nieuws</h2>
-            <p class="card-text">Aantal afleveringen: <strong>${totalNewsEpisodes}</strong><br/>
-            <strong>${newsPercentage.toFixed(1)}%</strong> van alle afleveringen is een nieuws aflevering.</p>
+            <p class="card-text">Aantal afleveringen: <strong>${totalNewsEpisodes}</strong></p>
+            <p class="card-text">De nieuws afleveringen duren opgeteld bij elkaar <strong>${totalNewsHours.toFixed(0)} uur</strong>.</p>
+            <p class="card-text"><strong>${newsPercentage.toFixed(1)}%</strong> van alle afleveringen is een nieuws aflevering.</p>
         `;
     })
     .catch(error => {
@@ -37,6 +46,14 @@ function calculateAchtergrondStatistics() {
     .then(data => {
         const achtergrondEpisodes = data.filter(episode => episode.categorie === 'achtergrond');
         const totalAchtergrondEpisodes = achtergrondEpisodes.length;
+        let totalAchtergrondMinutes = 0;
+
+        // Loop door de achtergond afleveringen en tel de tijd in minuten op
+        achtergrondEpisodes.forEach(episode => {
+            totalAchtergrondMinutes += parseInt(episode.tijd);
+        });
+        // Bereken het totale aantal uren
+        const totalAchtergrondHours = totalAchtergrondMinutes / 60;
 
         const totalEpisodes = data.length;
         const achtergrondPercentage = (totalAchtergrondEpisodes / totalEpisodes) * 100;
@@ -44,16 +61,18 @@ function calculateAchtergrondStatistics() {
         // Weergeef de statistieken
         const achtergrondStatisticsElement = document.getElementById('achtergrondStatistics');
         achtergrondStatisticsElement.innerHTML = `
-            <h2 class="h5 card-title">Achtergrond</h2>
-            <p class="card-text">Aantal afleveringen: <strong>${totalAchtergrondEpisodes}</strong><br/>
-            <strong>${achtergrondPercentage.toFixed(1)}%</strong> van alle afleveringen is een achtergrond aflevering.</p>
-        `;
+        <h2 class="h5 card-title">Achtergronden</h2>
+        <p class="card-text">Aantal afleveringen: <strong>${totalAchtergrondEpisodes}</strong></p>
+        <p class="card-text">De achtergrond afleveringen duren opgeteld bij elkaar <strong>${totalAchtergrondHours.toFixed(0)} uur</strong>.</p>
+        <p class="card-text"><strong>${achtergrondPercentage.toFixed(1)}%</strong> van alle afleveringen is een achtergrond aflevering.</p>
+    `;
     })
     .catch(error => {
         console.error('Error:', error);
         alert('Er is een fout opgetreden bij het laden van de gegevens.');
     });
 }
+
 function calculateReportageStatistics() {
     fetch('episodes.json')
     .then(response => {
@@ -65,6 +84,14 @@ function calculateReportageStatistics() {
     .then(data => {
         const reportageEpisodes = data.filter(episode => episode.categorie === 'reportage');
         const totalReportageEpisodes = reportageEpisodes.length;
+        let totalReportageMinutes = 0;
+
+        // Loop door de achtergond afleveringen en tel de tijd in minuten op
+        reportageEpisodes.forEach(episode => {
+            totalReportageMinutes += parseInt(episode.tijd);
+        });
+        // Bereken het totale aantal uren
+        const totalReportageHours = totalReportageMinutes / 60;
 
         const totalEpisodes = data.length;
         const reportagePercentage = (totalReportageEpisodes / totalEpisodes) * 100;
@@ -72,10 +99,11 @@ function calculateReportageStatistics() {
         // Weergeef de statistieken
         const reportageStatisticsElement = document.getElementById('reportageStatistics');
         reportageStatisticsElement.innerHTML = `
-            <h2 class="h5 card-title">Reportage</h2>
-            <p class="card-text">Aantal afleveringen: <strong>${totalReportageEpisodes}</strong><br/>
-            <strong>${reportagePercentage.toFixed(1)}%</strong> van alle afleveringen is een reportage.</p>
-        `;
+        <h2 class="h5 card-title">Reportages</h2>
+        <p class="card-text">Aantal afleveringen: <strong>${totalReportageEpisodes}</strong></p>
+        <p class="card-text">De reportages duren opgeteld bij elkaar <strong>${totalReportageHours.toFixed(0)} uur</strong>.</p>
+        <p class="card-text"><strong>${reportagePercentage.toFixed(1)}%</strong> van alle afleveringen is een reportage</p>
+    `;
     })
     .catch(error => {
         console.error('Error:', error);
@@ -93,6 +121,14 @@ function calculateInterviewStatistics() {
     .then(data => {
         const interviewEpisodes = data.filter(episode => episode.categorie === 'interview');
         const totalInterviewEpisodes = interviewEpisodes.length;
+        let totalInterviewMinutes = 0;
+
+        // Loop door de achtergond afleveringen en tel de tijd in minuten op
+        interviewEpisodes.forEach(episode => {
+            totalInterviewMinutes += parseInt(episode.tijd);
+        });
+        // Bereken het totale aantal uren
+        const totalInterviewHours = totalInterviewMinutes / 60;
 
         const totalEpisodes = data.length;
         const interviewPercentage = (totalInterviewEpisodes / totalEpisodes) * 100;
@@ -100,10 +136,11 @@ function calculateInterviewStatistics() {
         // Weergeef de statistieken
         const interviewStatisticsElement = document.getElementById('interviewStatistics');
         interviewStatisticsElement.innerHTML = `
-            <h2 class="h5 card-title">Interviews</h2>
-            <p class="card-text">Aantal afleveringen: <strong>${totalInterviewEpisodes}</strong><br/>
-            <strong>${interviewPercentage.toFixed(1)}%</strong> van alle afleveringen is een interview.</p>
-        `;
+        <h2 class="h5 card-title">Interviews</h2>
+        <p class="card-text">Aantal afleveringen: <strong>${totalInterviewEpisodes}</strong></p>
+        <p class="card-text">De interviews duren opgeteld bij elkaar <strong>${totalInterviewHours.toFixed(0)} uur</strong>.</p>
+        <p class="card-text"><strong>${interviewPercentage.toFixed(1)}%</strong> van alle afleveringen is een interview.</p>
+    `;
     })
     .catch(error => {
         console.error('Error:', error);
